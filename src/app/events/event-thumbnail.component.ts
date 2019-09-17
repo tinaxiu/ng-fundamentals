b40a2957c67f3e7e5f501e48066390076c8709f9
Tina Cao <MinyuTie.Cao@wolterskluwer.com>
2019-09-17 09:25:13 -0500

    ng

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter} from '@angular/core'
+import { Component, Input} from '@angular/core'
 //import { EventEmitter } from 'protractor'
 
 @Component(
@@ -6,19 +6,27 @@ import { Component, Input, Output, EventEmitter} from '@angular/core'
         selector:'event-thumbnail',
         template:`
             <div class="well hoverwell thumbnail">
-            <h2> {{event.name}}</h2>
-            <div>Date: {{event.date}}</div>
-            <div>Tima: {{event.time}}</div>
-            <div>Price: \${{event.price}}</div>
-            <div>
-                <span>Locations: {{event.location.address}}</span>
-                <span> &nbsp; </span>
-                <span>{{event.location.city}}</span>
-                <span> &nbsp; </span>
-                <span>{{event.location.Country}}</span>
+            <h2> {{event?.name}}</h2>
+            <div>Date: {{event?.date}}</div>
+            <div>Tima: {{event?.time}}</div>
+            <div>Price: \${{event?.price}}</div>
+            <div *ngIf="event?.location">
+                <span>Locations: {{event?.location?.address}}</span>
+                <span class="pad-left">{{event?.location?.city}}, {{event.location.Country}}</span>
+
+            </div>
+            <div *ngIf="event?.onlineUrl">
+                Online URL: {{event?.onlineUrl}}
             </div>
             </div>
-        `
+        `,
+        styles:[
+            `.pad-left {margin-left: 10px;}
+             .well div {color: #bbb;}
+             .thumbnail {min-height: 210px;}
+             `
+            
+        ]
 
     }
 )
@@ -26,9 +34,5 @@ import { Component, Input, Output, EventEmitter} from '@angular/core'
 export class EventThumbnailComponent
 {
     @Input() event:any
-    Someproperty: any = "some value"
-    logFoo()
-    {
-        console.log('foo')
-    }
+
 }
---
+++ src/app/nav/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+import { templateJitUrl } from '@angular/compiler';
+
+@Component({
+    selector:'nav-bar',
+    templateUrl:'./nav-bar.component.html',
+    styles:[
+        `
+        .nav.navbar-nav {font-size: 15px;}
+        #searchForm {margin-right: 100px;}
+        @media (max-with: 1200px) {#searchForm {display:none}}
+        `
+    ]
+})
+
+export class NavBarComponent
+{
+
+}
