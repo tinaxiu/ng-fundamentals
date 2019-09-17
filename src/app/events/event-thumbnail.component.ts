6748a05ee74dad518d19842ca489da8ec6174eb9
Tina Cao <MinyuTie.Cao@wolterskluwer.com>
2019-09-17 15:36:53 -0500

    6. linking to routes

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,23 +6,23 @@ declare let toastr
     {
         selector:'event-thumbnail',
         template:`
-            <div class="well hoverwell thumbnail">
-            <h2> {{event?.name}}</h2>
-            <div>Date: {{event?.date}}</div>
-            <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">Tima: {{event?.time}}
-                <span *ngSwitchCase="'8:00 am'"> (Early Start)</span>
-                <span *ngSwitchCase="'10:00 am'"> (Late Start)</span>
-                <span *ngSwitchDefault> (Normal Start)</span>            
-            </div>
-            <div>Price: \${{event?.price}}</div>
-            <div *ngIf="event?.location">
-                <span>Locations: {{event?.location?.address}}</span>
-                <span class="pad-left">{{event?.location?.city}}, {{event.location.Country}}</span>
+            <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
+                <h2> {{event?.name}}</h2>
+                <div>Date: {{event?.date}}</div>
+                <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">Tima: {{event?.time}}
+                    <span *ngSwitchCase="'8:00 am'"> (Early Start)</span>
+                    <span *ngSwitchCase="'10:00 am'"> (Late Start)</span>
+                    <span *ngSwitchDefault> (Normal Start)</span>            
+                </div>
+                <div>Price: \${{event?.price}}</div>
+                <div *ngIf="event?.location">
+                    <span>Locations: {{event?.location?.address}}</span>
+                    <span class="pad-left">{{event?.location?.city}}, {{event.location.Country}}</span>
 
-            </div>
-            <div *ngIf="event?.onlineUrl">
-                Online URL: {{event?.onlineUrl}}
-            </div>
+                </div>
+                <div *ngIf="event?.onlineUrl">
+                    Online URL: {{event?.onlineUrl}}
+                </div>
             </div>
         `,
         styles:[
